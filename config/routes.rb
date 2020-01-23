3814660287dc5d2d3006ff038697664160e369d4
Student <example@example.com>
2020-01-23 11:48:18 -0800

    adds sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+  # get '/users' => 'users#index'
 
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
